3a0baa0d753c3a36172ed27636b5a80372b77f33
rhennig <31764416+rhennigg@users.noreply.github.com>
2024-11-04 22:37:38 +0000

    atualização da pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Página inicial!</h1>
+    return <h1>Mudarei essa frase diariamente (até ter um projeto real publicado)! Dia 01 (06/11/24)</h1>
 }
 
 export default Home;
