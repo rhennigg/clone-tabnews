143370d2903476ba37fc7aea0239e7eb60720406
rhennig <31764416+rhennigg@users.noreply.github.com>
2024-11-08 13:35:12 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Mudarei essa frase diariamente (até ter um projeto real publicado)! Dia 01 (06/11/24)</h1>
+  return (
+    <h1>
+      Mudarei essa frase diariamente (até ter um projeto real publicado)! Dia 01
+      (06/11/24)
+    </h1>
+  );
 }
 
 export default Home;
