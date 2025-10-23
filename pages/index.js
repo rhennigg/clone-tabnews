1f569f1a241762a35a4842bb25caffd40861fc55
rhennig <31764416+rhennigg@users.noreply.github.com>
2025-10-23 01:07:58 +0000

    frase modificada

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       Mudarei essa frase diariamente (até ter um projeto real publicado)! Dia 01
-      (06/11/24)... Frase add 08/09/25
+      (06/11/24)... Frase add 08/09/25 Add 22/10/25
     </h1>
   );
 }
